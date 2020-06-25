eda0dbe29e23e40a80a34c297cdf66b722ee45bb
elijahscriptdev <elijahobominuru@gmail.com>
2020-06-25 16:15:56 +0100

    add modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,66 @@ import './css/main.css';
 
 import homepage from './home.js';
 import contact from './contact.js';
+import navBar from './navbar';
 // import menuTab from './menu.js';
-homepage();
-contact();
-menuTab();
+// homepage();
+// contact();
+// menuTab();
+// navBar()
+
+const content = document.getElementById('content');
+const container = document.createElement('div');
+container.setAttribute('id', 'container');
+
+const start = () => {
+    content.appendChild(navBar());
+    content.appendChild(container);
+    container.appendChild(homepage());
+};
+start();
+
+// const homePage = document.getElementById('home');
+// const menuPage = document.getElementById('menu');
+// const contactPage = document.getElementById('contact');
+// const logo = document.getElementById('logo');
+
+// const clickHome = () => {
+//     container.innerHTML = '';
+//     container.appendChild(home());
+//     homePage.classList.add('active');
+//     menuPage.classList.remove('active');
+//     contactPage.classList.remove('active');
+// };
+// clickHome();
+
+// const clickMenu = () => {
+//     container.innerHTML = '';
+//     container.appendChild(menu());
+//     menuPage.classList.add('active');
+//     homePage.classList.remove('active');
+//     contactPage.classList.remove('active');
+// };
+
+// const clickContact = () => {
+//     container.innerHTML = '';
+//     container.appendChild(contact());
+//     homePage.classList.remove('active');
+//     menuPage.classList.remove('active');
+//     contactPage.classList.add('active');
+// };
+
+// homePage.addEventListener('click', () => {
+//     clickHome();
+// });
+
+// logo.addEventListener('click', () => {
+//     clickHome();
+// });
+
+// menuPage.addEventListener('click', () => {
+//     clickMenu();
+// });
+
+// contactPage.addEventListener('click', () => {
+//     clickContact();
+// });
